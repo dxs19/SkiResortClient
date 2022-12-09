a6f69948621c76e9af8ca0f1e3c05e2fba64878d
Daniel Shull <danielshull121@gmail.com>
2022-12-09 13:26:36 -0500

    new details

--- src/pages/ResortDetails.js
+++ src/pages/ResortDetails.js
@@ -2,10 +2,11 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { Navigate, useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
-import { GetResortsById } from '../services/ResortServices'
 const ResortDetails = () => {
     let { id } = useParams()
     let navigate = useNavigate()
+
+
 
     const initialState = {
         resortId: id
@@ -13,7 +14,12 @@ const ResortDetails = () => {
     const [resorts, setResorts] = useState({})
     const [formState, setFormState] = useState(initialState)
 
-
+    const handleDelete = async (id) => {
+        await axios.delete(
+            `http://localhost:3001/api/resorts/${id}`
+        )
+        navigate('/resorts')
+    }
     useEffect(() => {
         const detailsCall = async () => {
             await axios
@@ -29,15 +35,18 @@ const ResortDetails = () => {
 
 
 
+
     return (
         <div>
-            <h3>Details Page:</h3>
+            <button onClick={() => navigate('/resorts')} className="back-btn">Back to Resorts</button>
+
             <h1>{resorts.name}</h1>
             <img src={resorts.image} />
             <h2>Resort Overview: {resorts.review}</h2>
             <h2>Location: {resorts.location}</h2>
             <h2>Height: {resorts.height}</h2>
             <h2>Number of Lifts: {resorts.runs}</h2>
+            {/* <button onClick={() => handleDelete(resorts.id)} className="delete-btn">Delete Resort</button> */}
         </div>
     )
 }
--- src/services/ResortServices.js
+++ src/services/ResortServices.js
@@ -8,6 +8,33 @@ export const GetResorts = async () => {
         throw error
     }
 }
+export const CreateResort = async (data) => {
+    try {
+        const res = await Client.post(`/resorts/addResort`, data)
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const UpdateResort = async (id) => {
+    try {
+        const res = await Client.put(`/resorts/${id}`)
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+
+export const DeleteResort = async (id) => {
+    try {
+        const res = await Client.delete(`/resorts/${id}`)
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
 
 export const GetResortsById = async (id) => {
     try {
